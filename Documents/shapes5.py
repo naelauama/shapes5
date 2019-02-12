0c94922c8a4b46bddef2de17e1930afeef818dad
csPython <cspython@AD25TF04NGF1J-42.local>
2019-02-12 11:39:36 -0500

    Added Rhombus in center

--- Documents/shapes5.py
+++ Documents/shapes5.py
@@ -1,4 +1,17 @@
 from graphics import*
 
-TriWin = GraphWin("Blue Triange", 300, 300)
-TriWin.setCoords(0, 0, 300, 300)
+Win = GraphWin("Blue Triange", 800, 800)
+Win.setCoords(0, 0, 800, 800)
+
+Tri = Polygon(Point(100,100), Point(150, 200), Point(200,100))
+Tri.setFill(color_rgb(30,30,230))
+Tri.draw(Win)
+
+PDiamond = Polygon(Point(400,524), Point(500, 424), Point(400,324), Point(300,424))
+PDiamond.setFill(color_rgb(255,105,180))
+PDiamond.draw(Win)
+
+
+Win.getMouse()
+Win.close()
+
