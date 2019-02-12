aeebb3f280e4830b8eb6ba89c59e0c28f66c1035
csPython <cspython@AD25TF04NGF1J-42.local>
2019-02-12 11:06:16 -0500

    First triange

--- Documents/shapes5.py
+++ Documents/shapes5.py
@@ -1,6 +1,11 @@
-from graphcs import*
+from graphics import*
 
-bTriWin = GraphWin("Blue Triange", 300, 300)
-bTriWin.setCoords(0, 0, 300, 300)
+TriWin = GraphWin("Blue Triange", 300, 300)
+TriWin.setCoords(0, 0, 300, 300)
 
-bTri = Polygon(Point(100,100), Point(150, 200)
+Tri = Polygon(Point(100,100), Point(150, 200), Point(200,100))
+Tri.setFill(color_rgb(30,30,230))
+Tri.draw(TriWin)
+
+TriWin.getMouse()
+TriWin.close()
