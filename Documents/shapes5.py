92c127bc9d6f5a5109b10539e4eba6045ff1efe5
csPython <cspython@AD25TF04NGF1J-42.local>
2019-02-12 11:09:26 -0500

    window created

--- Documents/shapes5.py
+++ Documents/shapes5.py
@@ -2,10 +2,3 @@ from graphics import*
 
 TriWin = GraphWin("Blue Triange", 300, 300)
 TriWin.setCoords(0, 0, 300, 300)
-
-Tri = Polygon(Point(100,100), Point(150, 200), Point(200,100))
-Tri.setFill(color_rgb(30,30,230))
-Tri.draw(TriWin)
-
-TriWin.getMouse()
-TriWin.close()
