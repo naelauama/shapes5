f4312d8ad6ab2d740b4e538c88941342fdab2d55
csPython <cspython@AD25TF04NGF1J-42.local>
2019-02-15 09:20:26 -0500

    added yellow square

--- Documents/shapes5.py
+++ Documents/shapes5.py
@@ -3,13 +3,13 @@ from graphics import*
 Win = GraphWin("Blue Triange", 800, 800)
 Win.setCoords(0, 0, 800, 800)
 
-Tri = Polygon(Point(100,100), Point(150, 200), Point(200,100))
+Tri = Polygon(Point(50,50), Point(100, 150), Point(150,50))
 Tri.setFill(color_rgb(30,30,230))
 Tri.draw(Win)
 
-PDiamond = Polygon(Point(400,524), Point(500, 424), Point(400,324), Point(300,424))
-PDiamond.setFill(color_rgb(255,105,180))
-PDiamond.draw(Win)
+Ysquare = Polygon(Point(50,750), Point(150, 750), Point(150,650), Point(50,650))
+Ysquare.setFill(color_rgb(255,255,0))
+Ysquare.draw(Win)
 
 
 Win.getMouse()
