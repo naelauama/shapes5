99d913c910d80a16e771921dcfcfe4ac89c6e2f5
csPython <cspython@AD25TF04NGF1J-42.local>
2019-02-25 10:37:23 -0500

    Diamond centered using variables

--- Documents/shapes5.py
+++ Documents/shapes5.py
@@ -1,11 +1,13 @@
 from graphics import*
 
-Win = GraphWin("Blue Triange", 800, 800)
+winX = int(800)
+winY = int(800)
+Win = GraphWin("5 Shapes", 800, 800)
 Win.setCoords(0, 0, 800, 800)
 
-Tri = Polygon(Point(50,50), Point(100, 150), Point(150,50))
-Tri.setFill(color_rgb(30,30,230))
-Tri.draw(Win)
+PTri = Polygon(Point(50,50), Point(100, 150), Point(150,50))
+PTri.setFill(color_rgb(249,149,132))
+PTri.draw(Win)
 
 Ysquare = Polygon(Point(50,750), Point(150, 750), Point(150,650), Point(50,650))
 Ysquare.setFill(color_rgb(255,255,0))
@@ -18,6 +20,13 @@ Trectange.draw(Win)
 Loval = Oval(Point(650, 50), Point(730,150))
 Loval.setFill(color_rgb(200,162,200))
 Loval.draw(Win)
+
+Bdiamond = Polygon(Point(winX / 2, winY / 2 + 110),
+                  Point(winX / 2 + 50, winY / 2),
+                  Point(winX / 2, winY / 2 - 110),
+                  Point(winX / 2 - 50, winY / 2))
+Bdiamond.setFill(color_rgb(100,149,237))
+Bdiamond.draw(Win)
 
 Win.getMouse()
 Win.close()
